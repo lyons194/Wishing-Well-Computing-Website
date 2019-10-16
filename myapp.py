0270e6bea1c1a87593740d08ec29cd08f56b9965
Sean Lyons <slyons494@gmail.com>
2019-10-16 11:43:36 -0600

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -5,5 +5,9 @@ app=Flask(__name__)
 def home():
     return render_template('home.html')
 
+@app.route('/option1')
+def wha():
+    print('TEST PAGE!!!')
+
 if __name__ == '__main__':
     app.run(debug=True)
